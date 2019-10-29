32c4f628259f8dffeb6a857f656e976247158a3f
wedeling <wouteredeling@gmail.com>
2019-10-29 16:00:34 +0100

    fixed bug that produces too good results

--- tests/lorenz96_neural_net.py
+++ tests/lorenz96_neural_net.py
@@ -1,16 +1,3 @@
-def animate(s):
-
-    print('Processing frame', s, 'of', sol.shape[0])
-    #add the periodic BC to current sample to create a closed plot
-    xlist = [theta, theta_Y]
-    ylist = [np.append(sol[s, :], sol[s, 0]), np.append(sol_Y[s].flatten(), sol_Y[s][0,0])]
-
-    #for index in range(0,1):
-    for lnum,line in enumerate(lines):
-        line.set_data(xlist[lnum], ylist[lnum]) # set data for each line separately. 
-
-    return lines
-
 def rhs_X(X, B):
     """
     Compute the right-hand side of X
@@ -119,6 +106,14 @@ def step_Y(Y_n, g_nm1, X_n):
     
     return Y_np1, g_n
 
+def wave_variance(X):
+    
+    X_hat = np.fft.rfft(X, axis=0, norm='ortho')
+    X_hat_mean = np.mean(X_hat, axis=0)
+    X_hat_var = np.mean(np.abs(X_hat - X_hat_mean)**2, axis=0)
+    
+    return X_hat_var
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -128,21 +123,21 @@ from itertools import chain
 
 plt.close('all')
 
-##Lorenz96 parameters
-#K = 18
-#J = 20
-#F = 10.0
-#h_x = -1.0
-#h_y = 1.0
-#epsilon = 0.5
-
-#trimodal Lorenz96 parameters
-K = 32
-J = 16
-F = 18.0
-h_x = -3.2
+#Lorenz96 parameters
+K = 18
+J = 20
+F = 10.0
+h_x = -1.0
 h_y = 1.0
 epsilon = 0.5
+
+##trimodal Lorenz96 parameters
+#K = 32
+#J = 16
+#F = 18.0
+#h_x = -3.2
+#h_y = 1.0
+#epsilon = 0.5
 
 dt = 0.01
 t_end = 1000.0
@@ -151,24 +146,25 @@ t = np.arange(0.0, t_end, dt)
 make_movie = False
 store = False
 train = True
-exact = False
 
 HOME = os.path.abspath(os.path.dirname(__file__))
 
 #load training data
 
 #NOTE IS NOT TRIMODAL, PROBABLY OVERWRITTEN
-store_ID = 'L96_trimodal'
+store_ID = 'L96'
 QoI = ['X_data', 'B_data']
 h5f = h5py.File(HOME + '/samples/' + store_ID + '.hdf5', 'r')
+
+print('Loading', HOME + '/samples/' + store_ID + '.hdf5')
 
 for q in QoI:
     print(q)
     vars()[q] = h5f[q][:]
-
+    
 feat_eng = es.methods.Feature_Engineering(X_data, B_data)
 
-lags = [[1]]
+lags = [[0]]
 max_lag = np.max(list(chain(*lags)))
 X_train, y_train = feat_eng.lag_training_data([X_data], lags = lags)
 
@@ -185,96 +181,39 @@ else:
 
 surrogate.get_n_weights()
 
-##equilibrium initial condition for X, zero IC for Y
-#X_n = np.ones(K)*F
-#X_n[10] += 0.01 #add small perturbation to 10th variable
-
 for i in range(max_lag):
     feat_eng.append_feat([X_data[i]])
 
-X_n = X_data[max_lag]
+X_n = X_data[max_lag+1]
+B = B_data[max_lag+1]
 
-#initial condition small-scale variables
-Y_n = np.zeros([J, K])
-B = h_x*np.mean(Y_n, axis=0)
+X_nm1 = X_data[max_lag]
+B_nm1 = B_data[max_lag]
 
 #initial right-hand sides
-f_nm1 = rhs_X(X_n, B)
-
-g_nm1 = np.zeros([J, K])
-for k in range(K):
-    g_nm1[:, k] = rhs_Y_k(Y_n, X_n[k], k)
+f_nm1 = rhs_X(X_nm1, B_nm1)
 
 #allocate memory for solutions
 sol = np.zeros([t.size, K])
-sol_Y = np.zeros([t.size, J, K])
 
 #start time integration
 idx = 0
 for t_i in t[max_lag:]:
     
-    if exact:
-        #solve small-scale equation
-        Y_n, g_nm1 = step_Y(Y_n, g_nm1, X_n)
-        #compute SGS term
-        B = h_x*np.mean(Y_n, axis=0)
-    else:
-        feat = feat_eng.get_feat_history()
-        B = surrogate.feed_forward(feat.reshape([1, feat.size])).flatten()
-        feat_eng.append_feat([X_n])
-
     #solve large-scale equation
     X_n, f_nm1 = step_X(X_n, f_nm1, B)
+    feat_eng.append_feat([X_n])
+
+    #ANN SGS solve
+    feat = feat_eng.get_feat_history()
+    B = surrogate.feed_forward(feat.reshape([1, feat.size])).flatten()
+
     #store solutions
     sol[idx, :] = X_n
-    sol_Y[idx, :] = Y_n
     idx += 1
     
     if np.mod(idx, 1000) == 0:
         print('t =', np.around(t_i, 1))
-    
-#plot results
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='polar')
-theta = np.linspace(0.0, 2.0*np.pi, K+1)
-theta_Y = np.linspace(0.0, 2.0*np.pi, J*K+1)
-#create a while in the middle to the plot, scales plot better
-ax.set_rorigin(-22)
-#remove set radial tickmarks, no labels
-ax.set_rgrids([-10, 0, 10], labels=['', '', ''])[0][1]
-ax.legend(loc=1)
-
-burn = 500
-X_data = sol[burn:, :]
-B_data = h_x*np.mean(sol_Y[burn:, :], axis=1)
-
-#store results
-if store == True:
-    #store results
-    samples = {}
-    store_ID = 'L96'
-    QoI = {'X_data', 'B_data'}
-    
-    for q in QoI:
-        samples[q] = eval(q)
-
-    post_proc = es.methods.Post_Processing()
-    post_proc.store_samples_hdf5(samples)
-
-#if True make a movie of the solution, if not just plot final solution
-if make_movie:
-
-    lines = []
-    legends = ['X', 'Y']
-    for i in range(2):
-        lobj = ax.plot([],[],lw=2, label=legends[i])[0]
-        lines.append(lobj)
-    
-    anim = FuncAnimation(fig, animate, frames=np.arange(0, sol.shape[0], 100), blit=True)    
-    anim.save('demo.gif', writer='imagemagick')
-else:
-    ax.plot(theta, np.append(sol[-1, :], sol[-1, 0]), label='X')    
-    ax.plot(theta_Y, np.append(sol_Y[-1, :].flatten(), sol_Y[-1, 0, 0]), label='Y')    
     
 #############   
 # Plot PDEs #
@@ -283,10 +222,11 @@ else:
 fig = plt.figure()
 ax = fig.add_subplot(111, xlabel=r'$X_k$')
 
-X_dom_surr, X_pde_surr = post_proc.get_pde(sol.flatten())
+post_proc = es.methods.Post_Processing()
+X_dom_surr, X_pde_surr = post_proc.get_pde(sol.flatten()[0:-1:10])
 X_dom, X_pde = post_proc.get_pde(X_data.flatten()[0:-1:10])
 
-ax.plot(X_dom, X_pde, '--k', label='L96')
+ax.plot(X_dom, X_pde, 'ko', label='L96')
 ax.plot(X_dom_surr, X_pde_surr, label='ANN')
 
 plt.yticks([])
@@ -307,7 +247,7 @@ R_sol = post_proc.auto_correlation_function(sol[:, 0], max_lag=1000)
 
 dom = np.arange(R_data.size)*dt
 
-ax.plot(dom, R_data, '--k', label='L96')
+ax.plot(dom, R_data, 'ko', label='L96')
 ax.plot(dom, R_sol, label='ANN')
 
 leg = plt.legend(loc=0)
@@ -326,7 +266,7 @@ C_sol = post_proc.cross_correlation_function(sol[:, 0], sol[:, 1], max_lag=1000)
 
 dom = np.arange(C_data.size)*dt
 
-ax.plot(dom, C_data, '--k', label='L96')
+ax.plot(dom, C_data, 'ko', label='L96')
 ax.plot(dom, C_sol, label='ANN')
 
 leg = plt.legend(loc=0)
