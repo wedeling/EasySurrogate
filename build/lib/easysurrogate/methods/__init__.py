8aad48d09d3cb62fe05bf965dad2a90c862d9dd3
wedeling <wouteredeling@gmail.com>
2019-12-17 13:22:17 +0100

    removed build dirs
    Former-commit-id: 695808387d5a6919028017e5d3be2645a5ed77fa
    Former-commit-id: 38686672ba0c7e47af8a7064c6e342a8b979c4d2

--- build/lib/easysurrogate/methods/__init__.py
+++
@@ -1,5 +0,0 @@
-from .resampling import Resampler
-from .NN import ANN
-from .SimpleBin import SimpleBin
-from .Feature_Engineering import Feature_Engineering
-from .Post_Processing import Post_Processing
