7fa669ca6bf770e5f5f6a4cac6ddfe8edb2a0a09
wedeling <wouteredeling@gmail.com>
2020-03-17 15:07:56 +0100

    removed unsed surrogate methods

--- easysurrogate/methods/__init__.py
+++ easysurrogate/methods/__init__.py
@@ -3,6 +3,4 @@ from .NN import ANN
 from .SimpleBin import SimpleBin
 from .Feature_Engineering import Feature_Engineering
 from .Post_Processing import Post_Processing
-from .RNN import RNN
-from .Input_Layer import Input_Layer
-from .CCM import CCM
+
