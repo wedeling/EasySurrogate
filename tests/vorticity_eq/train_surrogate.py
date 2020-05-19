6725cab34f463d742a117f8e9acd52f78477de14
Federica Gugole <federica.gugole@cwi.nl>
2020-05-19 15:44:29 +0200

    included if make_movie clause
    Former-commit-id: f890ede529e36ba303944b9e8d8ecf8589f9834d
    Former-commit-id: 1d2d3b4272b2c31a7968ae180325b109d30e977a

--- tests/vorticity_eq/train_surrogate.py
+++ tests/vorticity_eq/train_surrogate.py
@@ -6,11 +6,39 @@ from scipy.stats import norm, rv_discrete
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
+#####################
+# Other subroutines #
+#####################          
+
+def animate(i):
+    """
+    Generate a movie frame for the training verification (neural net only)
+    """
+    
+    if idx_max[0] == idx_data[0]:
+        c = 'b'
+    else:
+        c = 'r'
+    print('Test')
+    plt1 = ax1.vlines(np.arange(max(np.unique(feat_eng.binnumbers[:, 0]))), \
+    	ymin = np.zeros(max(np.unique(feat_eng.binnumbers[:, 0]))), ymax = o_i[0],
+                      colors = c, label=r'conditional pmf')
+    plt2 = ax1.plot(idx_data[0], 0.0, 'ro', label=r'SGS data')
+    print('Test1')
+    plt3 = ax2.plot(t[0:i], abs(y_train[0:i]), 'ro', alpha = 0.5, label=r'SGS data')
+    plt4 = ax2.plot(t[0:i], abs(samples[0:i, 0]), 'b', label='random sample')
+
+    if i == 0:
+        ax1.legend(loc=1, fontsize=9)
+        ax2.legend(loc=1, fontsize=9)
+
+    ims.append((plt1, plt2[0], plt4[0], plt3[0],))
+
 ####################
 # Simulation flags #
 ####################
-train = True             #train the network
-make_movie = False       #make a movie (of the training)
+train = False            #train the network
+make_movie = True        #make a movie (of the training)
 predict = False          #predict using the learned SGS term
 store = False            #store the prediction 
 make_movie_pred = False  #make a movie (of the prediction)
@@ -41,6 +69,16 @@ vort = vort.flatten()
 jac = jac.flatten()
 sgs = sgs.flatten()
 
+##################
+# Time parameters
+##################
+Omega = 7.292*10**-5 #time scale
+day = 24*60**2*Omega
+
+dt = 0.25*day 
+t0 = 365*day
+t_end = 4*365*day
+t = np.arange(t0, t_end, dt)
 #Lag features as defined in 'lags'
 lags = [[1, 10]]
 max_lag = np.max(list(chain(*lags)))
@@ -94,3 +132,47 @@ X_std = surrogate.X_std
     
 #number of features
 n_feat = surrogate.n_in
+
+#if True make a movie of the solution
+if make_movie:
+    
+    print('===============================')
+    print('Making movie...')
+
+    ims = []
+    fig = plt.figure(figsize=[8,4])
+    ax1 = fig.add_subplot(121, xlabel=r'bin number', ylabel=r'probability', 
+                          ylim=[-0.05, 1.05])
+    ax2 = fig.add_subplot(122, xlabel=r'time', ylabel=r'$B_k$')
+    plt.tight_layout()
+
+    #number of time steps to use in movie
+    n_movie = 500
+
+    #allocate memory
+    samples = np.zeros([n_movie, n_softmax],dtype=X_train.dtype)
+
+    #make movie by evaluating the network at TRAINING inputs
+    for i in range(n_movie):
+        
+        #draw a random sample from the network - gives conditional
+        #probability mass function (pmf)
+        o_i, idx_max, idx = surrogate.get_softmax(X_train[i].reshape([1, n_feat]))
+        idx_data = np.where(feat_eng.y_idx_binned[i] == 1.0)[0]
+        
+        #resample reference data based on conditional pmf
+        samples[i, :] = sampler.resample(idx)
+
+        if np.mod(i, 100) == 0:
+            print('i =', i, 'of', n_movie)
+
+        #create a single frame, store in 'ims'
+        animate(i)
+
+    #make a movie of all frame in 'ims'
+    im_ani = animation.ArtistAnimation(fig, ims, interval=80, 
+                                       repeat_delay=2000, blit=True)
+    plt.show()
+    # im_ani.save('./movies/test.mp4')
+
+    print('done')
