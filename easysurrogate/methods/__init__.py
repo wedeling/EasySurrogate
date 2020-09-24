4bb5609a42a89b4356a3fef19d03d0cd7f9f22a3
wouteredeling@gmail.com <wouteredeling@gmail.com>
2020-09-24 16:26:33 +0200

    removed old code

--- easysurrogate/methods/__init__.py
+++
@@ -1,5 +0,0 @@
-from .resampling import Resampler
-from .NN import ANN
-from .SimpleBin import SimpleBin
-from .Data_Engineering import Data_Engineering
-from .Post_Processing import Post_Processing
