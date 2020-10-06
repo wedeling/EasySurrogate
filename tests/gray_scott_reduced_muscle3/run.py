6740c9f92b00b44ab76c80483b0747ca8c3fd637
wouteredeling@gmail.com <wouteredeling@gmail.com>
2020-10-06 12:51:04 +0200

    coupled muscl easyvvuq for use on eagle

---
+++ tests/gray_scott_reduced_muscle3/run.py
@@ -0,0 +1,25 @@
+from libmuscle.runner import run_simulation
+from ymmsl import ComputeElement, Conduit, Configuration, Model, Settings
+
+from micro import reduced_sgs
+from macro import gray_scott_macro
+
+elements = [ComputeElement('macro', 'macro', [1]),
+            ComputeElement('micro', 'micro', [1])]
+
+conduits = [Conduit('macro.state_out', 'micro.state'),
+            Conduit('micro.sgs', 'macro.state_in')]
+
+model = Model('gray_scott_reduced', elements, conduits)
+
+settings = Settings({'micro.t_max': 0.1, 'micro.dt': 0.1, 'N_Q': 2, 
+                     'N_LF': 128, 
+                     'macro[0].feed': 0.02, 
+                     'macro[0].kill': 0.05,
+                     })
+
+configuration = Configuration(model, settings)
+
+implementations = {'macro': gray_scott_macro, 'micro': reduced_sgs}
+
+run_simulation(configuration, implementations)
