fix classification error computation
Former-commit-id: b025e66c70c6f4c0766259682d6843554539b8cb
Former-commit-id: 2432e241c3be87d6258387c22978b67971ad194d

tests/lorenz96_qsn/train_surrogate.py
@@ -28,4 +28,4 @@ campaign.save_state()
 
 # QSN analysis object
 analysis = es.analysis.QSN_analysis(surrogate)
-analysis.get_classification_error(features[0:1000], target[0:1000])
+analysis.get_classification_error(index=np.arange(0, 10000))