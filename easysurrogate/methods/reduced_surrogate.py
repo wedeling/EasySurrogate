476966e55fa1d23e17ef6a066cd2b667fd5ecd6f
wouteredeling@gmail.com <wouteredeling@gmail.com>
2020-12-23 19:09:36 +0100

    finilized dQ training script
    Former-commit-id: f59c0c59f3167e0d9de261dc8204294785caea92
    Former-commit-id: d2c5727b3991ad7b046019d73473b9140de8f8c4

--- easysurrogate/methods/reduced_surrogate.py
+++ easysurrogate/methods/reduced_surrogate.py
@@ -1,7 +1,3 @@
-import numpy as np
-from ..campaign import Campaign
-import easysurrogate as es
-
 """
 ===============================================================================
 CLASS FOR A REDUCED SURROGATE FOR SPECTRAL MODELS
@@ -12,10 +8,31 @@ models by physical constraints, Computer & Fluids, 2020.
 ===============================================================================
 """
 
+import numpy as np
+from ..campaign import Campaign
+
 
 class Reduced_Surrogate(Campaign):
+    """
+    Reduced Surrogate class
+    """
 
-    def __init__(self, n_qoi, n_model_1d, **kwargs):
+    def __init__(self, n_qoi, n_model_1d):
+        """
+        Create a Reduced Surrogate object.
+
+        Parameters
+        ----------
+        n_qoi : int
+            The number of integrated QoI to track.
+        n_model_1d : int
+            The number of grid points in one direction.
+
+        Returns
+        -------
+        None.
+
+        """
         self.n_qoi = n_qoi
         self.n_model_1d = n_model_1d
         print('Creating Reduced Surrogate Object')
@@ -30,27 +47,30 @@ class Reduced_Surrogate(Campaign):
 
         Parameters
         ----------
-        V : TYPE
-            DESCRIPTION.
-        qois_ref : TYPE
-            DESCRIPTION.
-        qois_model : TYPE
-            DESCRIPTION.
+        V : list
+            A list containing the Fourier coefficients of reduced basis functions V_i.
+        qois_ref : array
+            The reference statistics at a given time step.
+        qois_model : array
+            The same statistics computed by the lower-resolution model.
 
         Returns
         -------
-        TYPE
-            DESCRIPTION.
+        dict
+            A dictionary containing:
+            'sgs_hat': the reduced subgrid-scale term,
+            'c_ij': the coefficients of the linear system A_i * c_i = b_i (Eq 20-21 in the paper),
+            'inner_prods': the inner products (V_i, T_{i,j}) (Eq 21),
+            'src_Q': the inner product (V_i, P_i) that is part of a source term of Q (Eq 23),
+            'tau': the multiplier of (V_i, P_i) (Eq 23).
 
         """
 
-        if not isinstance(qois_ref, np.ndarray) or not isinstance(qois_model, np.ndarray):
-            print('Training requires QoI data stored in numpy array')
-            return
+        assert isinstance(qois_ref, np.ndarray) and isinstance(qois_model, np.ndarray), \
+            'Training requires QoI data stored in numpy array'
 
-        if not qois_ref.size == self.n_qoi or not qois_model.size == self.n_qoi:
-            print('size dQ_data must be %d' % self.n_qoi)
-            return
+        assert qois_ref.size == self.n_qoi and qois_model.size == self.n_qoi, \
+            'size dQ_data must be %d' % self.n_qoi
 
         V_hat = np.zeros([self.n_qoi, self.n_model_1d, self.n_model_1d]) + 0.0j
         for i in range(self.n_qoi):
@@ -61,18 +81,38 @@ class Reduced_Surrogate(Campaign):
         return self.reduced_r(V_hat, dQ_data)
 
     def predict(self):
+        """
+        Not yet implemented.
+
+        Returns
+        -------
+        NotImplementedError : TYPE
+            DESCRIPTION.
+
+        """
         return NotImplementedError
 
     def save_state(self):
         """
-        Save the state of the QSN surrogate to a pickle file
+        Save the state of the reduced surrogate to a pickle file.
+
+        Returns
+        -------
+        None.
+
         """
+
         state = self.__dict__
         super().save_state(state=state, name=self.name)
 
     def load_state(self):
         """
-        Load the state of the QSN surrogate from file
+        Load the state of the reduced surrogate from file.
+
+        Returns
+        -------
+        None.
+
         """
         super().load_state(name=self.name)
 
@@ -83,6 +123,23 @@ class Reduced_Surrogate(Campaign):
     def reduced_r(self, V_hat, dQ):
         """
         Compute the reduced SGS term
+
+        Parameters
+        ----------
+        V_hat : list
+            A list containing the Fourier coefficients of reduced basis functions V_i.
+        dQ : array
+            The difference in the QoI between the high and low resolution model.
+
+        Returns
+        -------
+        reduced_dict : dict
+            A dictionary containing:
+            'sgs_hat': the reduced subgrid-scale term,
+            'c_ij': the coefficients of the linear system A_i * c_i = b_i (Eq 20-21 in the paper),
+            'inner_prods': the inner products (V_i, T_{i,j}) (Eq 21),
+            'src_Q': the inner product (V_i, P_i) that is part of a source term of Q (Eq 23),
+            'tau': the multiplier of (V_i, P_i) (Eq 23).
         """
 
         # compute the T_ij basis functions
@@ -102,7 +159,7 @@ class Reduced_Surrogate(Campaign):
         # compute the coefficients c_ij
         inner_prods = self.inner_products(V_hat)
 
-        c_ij = self.compute_cij_using_V_hat(V_hat, inner_prods)
+        c_ij = self.compute_cij_using_V_hat(inner_prods)
 
         sgs_hat = 0.0
 
@@ -117,7 +174,7 @@ class Reduced_Surrogate(Campaign):
                 P_hat_i -= c_ij[i, j] * T_hat[i, j + 1]
 
             # (V_i, P_i) integral
-            src_Q_i = self.compute_int(V_hat[i], P_hat_i, self.n_model_1d)
+            src_Q_i = compute_int(V_hat[i], P_hat_i, self.n_model_1d)
 
             # compute tau_i = Delta Q_i/ (V_i, P_i)
             tau_i = dQ[i] / src_Q_i
@@ -134,11 +191,22 @@ class Reduced_Surrogate(Campaign):
 
         return reduced_dict
 
-    def compute_cij_using_V_hat(self, V_hat, inner_prods):
+    def compute_cij_using_V_hat(self, inner_prods):
         """
-        compute the coefficients c_ij of P_i = T_{i,1} - c_{i,2}*T_{i,2}, - ...
-        """
+        Compute the coefficients c_ij of P_i = T_{i,1} - c_{i,2}*T_{i,2}, - ... (Eq 16, 20 and 21)
 
+        Parameters
+        ----------
+        inner_prods : array
+            A matrix containing all inner products (V_i, T_{i,j}).
+
+        Returns
+        -------
+        c_ij : array
+            The coefficients of the subgrid-scale basis functions (Eq 16, 20 and 21) at the
+            current time step.
+
+        """
         c_ij = np.zeros([self.n_qoi, self.n_qoi - 1])
 
         for i in range(self.n_qoi):
@@ -166,14 +234,44 @@ class Reduced_Surrogate(Campaign):
     def inner_products(self, V_hat):
         """
         Compute all the inner products (V_i, T_{i,j})
+
+        Parameters
+        ----------
+        V_hat : list
+            A list containing the Fourier coefficients of reduced basis functions V_i.
+
+        Returns
+        -------
+        inner_prods : array
+            A matrix containing all inner products (V_i, T_{i,j}).
+
         """
         V_hat = V_hat.reshape([self.n_qoi, self.n_model_1d**2])
 
-        return np.dot(V_hat, np.conjugate(V_hat).T) / self.n_model_1d**4
+        # TODO: I added the .real to get rid of a warning. Should be ok, but check if this does
+        # not yield problems
+        return np.dot(V_hat, np.conjugate(V_hat).T).real / self.n_model_1d**4
 
-    def compute_int(self, X1_hat, X2_hat, N):
-        """
-        Compute the integral of X1*X2 using the Fourier expansion
-        """
-        integral = np.dot(X1_hat.flatten(), np.conjugate(X2_hat.flatten())) / N**4
-        return integral.real
+
+def compute_int(X1_hat, X2_hat, N):
+    """
+    Compute the integral of X1*X2 over the spatial domain using the Fourier expansion,
+    see section 4.4 of the paper.
+
+    Parameters
+    ----------
+    X1_hat : array (complex)
+        The Fourier coefficients of X1.
+    X2_hat : array (complex)
+        The Fourier coefficients of X2.
+    N : int
+        The number of points in one spatial direction.
+
+    Returns
+    -------
+    float
+        The value of the integral.
+
+    """
+    integral = np.dot(X1_hat.flatten(), np.conjugate(X2_hat.flatten())) / N**4
+    return integral.real
