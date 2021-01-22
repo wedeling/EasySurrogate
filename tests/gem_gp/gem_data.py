bc631912fd70507867ec305bd9baab21550a9af4
YehorYudin <yehor.yudin@ipp.mpg.de>
2021-01-22 17:14:06 +0100

    GP test now have three separate scripts: to get data, to train a surrogate, to analyse a surrogate

---
+++ tests/gem_gp/gem_data.py
@@ -0,0 +1,41 @@
+import numpy as np
+import csv
+
+import easysurrogate as es
+
+features_names = ['te_value', 'ti_value', 'te_ddrho', 'ti_ddrho']
+target_names = ['te_transp_flux', 'ti_transp_flux']
+
+def load_csv_file():
+    N_runs = 625
+    frac_train = 0.5
+    input_dim = 4
+    output_dim = 2
+
+    input_samples = np.zeros((N_runs, input_dim))
+    output_samples = np.zeros((N_runs, output_dim))
+
+    input_file = 'gem_data_625.txt'
+
+    with open(input_file, 'r') as inputfile:
+        datareader = csv.reader(inputfile, delimiter=',')
+        i = 0
+        for row in datareader:
+            input_samples[i] = row[0:input_dim]
+            output_samples[i] = row[input_dim:input_dim+output_dim]
+            i = i + 1
+
+    data = {}
+    data['te_value'] = input_samples[:, 0].reshape(-1, 1)
+    data['ti_value'] = input_samples[:, 1].reshape(-1, 1)
+    data['te_ddrho'] = input_samples[:, 2].reshape(-1, 1)
+    data['ti_ddrho'] = input_samples[:, 3].reshape(-1, 1)
+    data['te_transp_flux'] = output_samples[:, 0].reshape(-1, 1)
+    data['ti_transp_flux'] = output_samples[:, 1].reshape(-1, 1)
+
+    return data
+
+# get dat ato a hfd5
+campaign = es.Campaign(load_state=False)
+data = load_csv_file()
+campaign.store_data_to_hdf5(data, file_path='gem_data_625.hdf5')
---
+++ tests/gem_gp/train_model.py
@@ -0,0 +1,23 @@
+import timeit
+import numpy as np
+import csv
+
+import easysurrogate as es
+
+features_names = ['te_value', 'ti_value', 'te_ddrho', 'ti_ddrho']
+target_names = ['te_transp_flux', 'ti_transp_flux']
+
+# Create a campaign object
+campaign = es.Campaign(load_state=False)
+
+data_frame = campaign.load_hdf5_data(file_path='gem_data_625.hdf5')
+#features = [np.array(data_frame[k]).transpose().reshape((-1, 1)) for k in features_names if k in data_frame]
+features = [data_frame[k] for k in features_names if k in data_frame]
+#target = np.array([data_frame[k] for k in target_names if k in data_frame]).transpose()
+target = np.concatenate([data_frame[k] for k in target_names if k in data_frame], axis=1)
+
+# Create a surrogate and its app
+surrogate = es.methods.GP_Surrogate()
+surrogate.train(features, target, test_frac=0.5)
+campaign.add_app(name='gp_campaign', surrogate=surrogate)
+campaign.save_state()
