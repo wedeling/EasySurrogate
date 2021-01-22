5bc03bb521451a05bb84d05cececee1e2fafa3bc
YehorYudin <yehor.yudin@ipp.mpg.de>
2021-01-22 17:14:06 +0100

    GP test now have three separate scripts: to get data, to train a surrogate, to analyse a surrogate
    Former-commit-id: bc631912fd70507867ec305bd9baab21550a9af4
    Former-commit-id: 42e38f6f4d5a15bc4f1b7bbc23f8d97a09717f6b

--- tests/gem_gp/gem_gp.py
+++ tests/gem_gp/gem_gp.py
@@ -5,18 +5,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-N_runs = 625
-frac_train = 0.5
-input_dim = 4
-output_dim = 2
+import easysurrogate as es
 
-input_samples = np.zeros((N_runs, input_dim))
-output_samples = np.zeros((N_runs, output_dim))
+features_names = ['te_value', 'ti_value', 'te_ddrho', 'ti_ddrho']
+target_names = ['te_transp_flux', 'ti_transp_flux']
 
-with open('', 'r') as inputfile:
-    datareader = csv.reader(inputfile, delimeter=',')
-    i = 0
-    for row in datareader:
-        input_samples[i] = row[0:input_dim-1]
-        output_samples[i] = row[input_dim:input_dim+output_dim]
-        i = i + 1
+# Get the simulation data for the model
+
+# datadict = load_csv_file() # if only csv file
+# campaign.store_data_to_hdf5(data, file_path='gem_data_625.hdf5') # if only csv file
+
+### PREPARING MODEL TO USE
+#load pre-trained campaign
+campaign = es.Campaign(load_state=True, file_path='gem_gp_model.pickle')
+data_frame = campaign.load_hdf5_data(file_path='gem_data_625.hdf5')
+features = [np.array(data_frame[k]).transpose().reshape((-1, 1)) for k in features_names if k in data_frame]
+target = np.array([data_frame[k] for k in target_names if k in data_frame]).transpose()
+
+analysis = es.analysis.GP_analysis(campaign.surrogate)
+analysis.get_regression_error()
