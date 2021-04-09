dab30f73b7d44d52298db1c949d2515911f36edf
Federica Gugole <federica.gugole@cwi.nl>
2021-04-09 15:37:50 +0200

    add the possibility to use lagged features
    Former-commit-id: 3e2fabbf79d83f2359e3b8fbbb4d702637100e90
    Former-commit-id: 5a1a4b3cd57ff774a55e46e86a256a4ed192b382

--- tests/lorenz96_bma/train_surrogate_LR.py
+++ tests/lorenz96_bma/train_surrogate_LR.py
@@ -17,11 +17,41 @@ campaign = es.Campaign()
 data_frame = campaign.load_hdf5_data()
 
 # supervised training data set
-features = data_frame['X_data']
-features = features.flatten() # don't care of the specific X_n; just want a surrogate that makes a "good" single estimate for all k
+X_data = data_frame['X_data']
+B_data = data_frame['B_data']
 
-target = data_frame['B_data']
-target = target.flatten() # don't care of the specific X_n; just want a surrogate that makes a "good" single estimate for all k
+lags = [] #[np.arange(start=1,stop=2,step=1)] # remember that the ending point of the interval is not included!
+
+if len(lags) == 0: 
+    # no lagged features
+    print('No lagged features.')
+    features = X_data.flatten() # don't care of the specific X_n; just want a surrogate that makes a "good" single estimate for all k
+    target = B_data.flatten()
+else:
+    # check for negative lags
+    if any(lag<0 for lag in lags):
+        print('Error: defined negative lag.')
+    else:
+        # consider also lagged features
+        max_lag = np.max(lags)
+        if max_lag == 0:
+            # no lagged features
+            print('No lagged features.')
+            features = X_data.flatten()
+            target = B_data.flatten()
+        else:
+            print('Considering also lagged features.')
+            target = B_data[max_lag:].flatten()
+        
+            L = len(X_data.flatten()) - max_lag*len(X_data[0])
+            features = np.zeros((L,len(lags)+1))
+            idx = 0
+            features[:,idx] = X_data[max_lag:,:].flatten()
+            for lag in lags:
+                lag = np.int(lag)
+                idx += 1
+                features[:,idx] = X_data[max_lag-lag:-lag,:].flatten()
+        
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -32,8 +62,12 @@ from sklearn.preprocessing import StandardScaler
 sc_f = StandardScaler()
 sc_t = StandardScaler()
 
-features_train = sc_f.fit_transform(features_train.reshape(-1,1))
-features_test = sc_f.transform(features_test.reshape(-1,1))
+if len(lags) == 0 or max_lag == 0:
+    features_train = features_train.reshape(-1,1)
+    features_test = features_test.reshape(-1,1)
+
+features_train = sc_f.fit_transform(features_train)
+features_test = sc_f.transform(features_test)
 
 target_train = sc_t.fit_transform(target_train.reshape(-1,1))
 target_test = sc_t.transform(target_test.reshape(-1,1))
@@ -68,19 +102,17 @@ print('Coefficient of determination: %.2f'
       % r2_score(target_test, target_pred))
 
 
-"""
-Plot true data vs regressor
-"""
-plt.figure()
-plt.scatter(sc_f.inverse_transform(features_train), sc_t.inverse_transform(target_train), color = 'red', label='Training samples')
-plt.scatter(sc_f.inverse_transform(features_test), sc_t.inverse_transform(target_test), color = 'purple', label='Test samples')
-plt.plot(sc_f.inverse_transform(features_test), sc_t.inverse_transform(target_pred), color = 'blue', label='Predictions')
-plt.title('How good is my regressor?')
-plt.xlabel('Features')
-plt.ylabel('Target')
-plt.legend(loc='best')
-plt.show()
-
+if len(lags) == 0 or max_lag == 0:
+    # Plot data vs linear regression
+    plt.figure() 
+    plt.scatter(sc_f.inverse_transform(features_train), sc_t.inverse_transform(target_train), color = 'red', label='Training samples')
+    plt.scatter(sc_f.inverse_transform(features_test), sc_t.inverse_transform(target_test), color = 'purple', label='Test samples')
+    plt.plot(sc_f.inverse_transform(features_test), sc_t.inverse_transform(target_pred), color = 'blue', label='Predictions')
+    plt.title('How good is my regressor?')
+    plt.xlabel('Features')
+    plt.ylabel('Target')
+    plt.legend(loc='best')
+    plt.show()
 
 campaign.add_app(name='test_campaign_LR', surrogate=regressor)
 campaign.add_scalers(name='test_campaign_LR', scaler_features=sc_f, scaler_target=sc_t)
