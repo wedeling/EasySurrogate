0c96bc1d2bc796969e079dccbf36a763662c96c9
YehorYudin <yehor.yudin@ipp.mpg.de>
2021-04-28 12:22:44 +0200

    Added MOGP surrogate implementation

--- easysurrogate/methods/GP.py
+++ easysurrogate/methods/GP.py
@@ -9,6 +9,9 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel, Matern, RBF, ConstantKernel
 from sklearn.metrics import mean_squared_error as mse
 
+import mogp_emulator as mogp
+from mogp_emulator import GaussianProcess
+
 
 class GP:
 
@@ -21,18 +24,17 @@ class GP:
                  length_scale=1.0,
                  prefactor=True,
                  bias=False,
-                 noize=True,
+                 noize=1e-8,
                  save=True,
                  load=False,
                  name='GP',
                  on_gpu=False,
+                 backend='scikit-learn',
                  standardize_X=True,
                  standardize_y=True,
                  **kwargs):
 
-        #self.X = X
-        #self.y = y
-
+        self.backend = backend
         self.n_train = X.shape[0]
 
         try:
@@ -47,29 +49,64 @@ class GP:
 
         self.on_gpu = on_gpu
 
-        self.kernel = ConstantKernel(constant_value=1.0, constant_value_bounds=(1e-6, 1e+6))
+        # sciki-learn specific part
+        if self.backend == 'scikit-learn':
+            self.kernel = ConstantKernel(constant_value=1.0, constant_value_bounds=(1e-6, 1e+6))
 
-        if kernel == 'Matern':
-            #self.kernel = Matern(length_scale=[length_scale]*self.n_in)
-            self.kernel *= Matern()
-        elif kernel == 'RBF':
-            self.kernel *= RBF(length_scale=[length_scale]*self.n_in, length_scale_bounds=[1e-4, 1e+4])
+            if kernel == 'Matern':
+                #self.kernel = Matern(length_scale=[length_scale]*self.n_in)
+                self.kernel *= Matern()
+            elif kernel == 'RBF':
+                self.kernel *= RBF(length_scale=[length_scale]*self.n_in, length_scale_bounds=[1e-4, 1e+4])
 
-        if bias:
-            self.kernel += ConstantKernel(constant_value=1.0, constant_value_bounds=(1e-5, 1e+5))
+            if bias:
+                self.kernel += ConstantKernel(constant_value=1.0, constant_value_bounds=(1e-5, 1e+5))
 
-        if noize:
-            self.kernel += WhiteKernel(noise_level=0.1, noise_level_bounds=(1e-09, 1e+2))
+            if noize is not False:
+                self.kernel += WhiteKernel(noise_level=noize, noise_level_bounds=(noize*1e-3, noize*1e+3))
 
-        self.instance = GaussianProcessRegressor(kernel=self.kernel, n_restarts_optimizer=5, normalize_y=True)  #, random_state=42
+            self.instance = GaussianProcessRegressor(kernel=self.kernel, n_restarts_optimizer=5, normalize_y=True)  #, random_state=42
+
+        # MOGP specific part
+        if self.backend == 'mogp':
+
+            y = y.reshape(-1)
+
+            kernel_argument = ''
+
+            if kernel == 'Matern':
+                kernel_argument += 'Matern52'
+
+            if kernel == 'RBF':
+                kernel_argument += 'SquaredExponential'
+
+            if noize is not False:
+                noize_argument = noize
+
+            if bias:
+                raise NotImplementedError('Non-stationary kernels are not implemented in MOGP')
+
+            self.instance = GaussianProcess(X, y, kernel=kernel_argument, nugget=noize_argument)
+
+        else:
+            raise NotImplementedError('Currently supporting only scikit-learn and mogp backend')
 
         self.train(X, y)
 
     def train(self, X, y):
-        self.instance.fit(X, y)
+        if self.backend == 'scikit-learn':
+            self.instance.fit(X, y)
+        if self.backend == 'mogp':
+            self.instance = mogp.fit_GP_MAP(self.instance)
 
     def predict(self, X_i):
-        m, v = self.instance.predict(X_i, return_std=True)  # for single sample X_i should be nparray(1,n_feat)
+        if self.backend == 'scikit-learn':
+            # for single sample X_i should be nparray(1, n_feat)
+            m, v = self.instance.predict(X_i, return_std=True)
+        if self.backend == 'mogp':
+            m, v, d = self.instance.predict(X_i)
+        else:
+            raise NotImplementedError('Non-stationary kernels are not implemented in MOGP')
         return m, v
 
     def forward(self, X_i):  # for no cases when required different from predict at GP case
