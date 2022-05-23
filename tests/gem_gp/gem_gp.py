b66b63e7bb84dbf8e33e45dc05d7e5f4a0133356
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2022-05-23 19:05:06 +0200

    Now GPs can analyse cases when entire dataset was used for training

--- tests/gem_gp/gem_gp.py
+++ tests/gem_gp/gem_gp.py
@@ -12,35 +12,54 @@ target_name_selected = target_names
 # PREPARING MODEL TO USE
 # load pre-trained campaign
 
-# 1) case for data from single flux tube GEM UQ campaign
+# 1) Case for data from single flux tube GEM UQ campaign
 #campaign = es.Campaign(load_state=True, file_path='gp_gem_625.pickle')
 #data_frame = campaign.load_hdf5_data(file_path='gem_data_625.hdf5')
 #data_frame_train = campaign.load_hdf5_data(file_path='gem_data_625.hdf5')
 
-# 2) case for data from a MFW production run
+# 2) Case for data from a MFW production run
 #campaign = es.Campaign(load_state=True, file_path='skl_gem_500_wf_1405_opt.pickle')
 #data_frame = campaign.load_hdf5_data(file_path='gem_workflow_500.hdf5')
 #data_frame_train = campaign.load_hdf5_data(file_path='gem_workflow_500.hdf5')
 
-# 3) case for data generated from single flux tube GEM0 with 4 parameters (LHD, with a wrapper)
+# 3) Case for data generated from single flux tube GEM0 with 4 parameters (LHD, with a wrapper)
 #campaign = es.Campaign(load_state=True, file_path='gem0_lhc.pickle')
 #data_frame = campaign.load_hdf5_data(file_path='gem_workflow_500.hdf5')
 #data_frame_train = campaign.load_hdf5_data(file_path='gem_workflow_500.hdf5')
 
-# 4) case for from single flux tube GEM0 with 2 parameters (LHD, with a wrapper)
-features_names_selected = [features_names[2], features_names[3]]
-target_name_selected = [target_names[1]]
-campaign = es.Campaign(load_state=True, file_path='gp_model_10pperctrset_plus1oseqsamples.pickle')
-data_frame = campaign.load_hdf5_data(file_path='gem0_lhc_256.hdf5')
-data_frame_train = campaign.load_hdf5_data(file_path='gem0_lhc_256.hdf5')
+# 4) Case for from single flux tube GEM0 with 2 parameters (LHD, with a wrapper)
+#features_names_selected = [features_names[2], features_names[3]]
+#target_name_selected = [target_names[1]]
+#campaign = es.Campaign(load_state=True, file_path=
+#                                        #'gp_model_10pperctrset_plus1oseqsamples.pickle'
+#                                         'model_190522.pickle'
+#                      )
+#data_frame = campaign.load_hdf5_data(file_path='gem0_lhc_256.hdf5')
+#data_frame_train = campaign.load_hdf5_data(file_path='gem0_lhc_256.hdf5')
 
-# getting the data
+# 5) Case from single flux tube GEM UQ campaign (4 parameters, tensor product of grid with 2 points per DoF)
+features_names_selected = features_names
+target_name_selected = [target_names[1]]
+campaign = es.Campaign(load_state=True, file_path=
+                                         #'mode_gem16_200522.pickle'
+                                          'model_05split_230522.pickle'
+                      )
+data_frame = campaign.load_hdf5_data(file_path='gem_uq_16.hdf5')
+data_frame_train = campaign.load_hdf5_data(file_path='gem_uq_16.hdf5')
+
+# To use or not to use analysis of sequential design results
+SEQDES = False
+
+# Getting the data
 features_train = [data_frame_train[k] for k in features_names_selected if k in data_frame_train]
 target_train = np.concatenate([data_frame_train[k]
                               for k in target_name_selected if k in data_frame_train], axis=1)
 
 feat_train, targ_train, feat_test, targ_test = campaign.surrogate.feat_eng.\
     get_training_data(features_train, target_train, index=campaign.surrogate.feat_eng.train_indices)
+
+#feat_test = feat_train ###DEBUG
+#targ_test = targ_train ###DEBUG
 
 features = [data_frame[k] for k in features_names_selected if k in data_frame]
 target = np.concatenate([data_frame[k] for k in target_name_selected if k in data_frame], axis=1)
@@ -49,10 +68,14 @@ target = np.concatenate([data_frame[k] for k in target_name_selected if k in dat
 analysis = es.analysis.GP_analysis(campaign.surrogate)
 
 # SEQ DES
-analysis.plot_2d_design_history(x_test=feat_test, y_test=targ_test)
+if SEQDES:
+    analysis.plot_2d_design_history(x_test=feat_test, y_test=targ_test)
 
 #analysis.get_regression_error(np.concatenate([feat_train, feat_test], axis=0), np.concatenate([targ_train, targ_test], axis=0))
-analysis.get_regression_error(feat_test, targ_test, feat_train, targ_train, )
+
+analysis.get_regression_error(feat_test, targ_test, feat_train, targ_train, 
+                              #index=[i for i in range(16)] #DEBUG
+                             )
 
 # Cross-correlation functions
 r = analysis.auto_correlation_function(targ_test, 5)
@@ -90,3 +113,4 @@ analysis.plot_pdfs(
         'tevl ts',
         'tivl tr',
         'tivl ts'])
+
