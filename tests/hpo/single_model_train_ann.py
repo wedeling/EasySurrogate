3a7d6e98f0462b01b32fa2c7612dad36fd99285c
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2023-12-13 19:33:04 +0100

    HPO: ANNs now use EasyVVUQ for hyper-parameter grid search

---
+++ tests/hpo/single_model_train_ann.py
@@ -0,0 +1,94 @@
+#from py import process
+import easysurrogate as es
+import sys
+import json
+import numpy as np
+
+print('> Entering the training script')
+np.random.seed(42)
+
+# Read the current hyperparameter values
+json_input = sys.argv[1]
+with open(json_input, "r") as f:
+    inputs = json.load(f)
+
+# Read the flux tube number
+ft = sys.argv[2]
+
+print('Inputs: {0}'.format(inputs))
+
+# Input and output names
+features_names = ['te_value', 'ti_value', 'te_ddrho', 'ti_ddrho']
+target_names = ['te_transp_flux', 'ti_transp_flux', 'te_transp_flux_std', 'ti_transp_flux_std']
+features_names_selected = [features_names[0], features_names[1], features_names[2], features_names[3]]
+
+# Chose target value - train for 2 QoIs
+#target_name_selected = [target_names[1]]
+target_name_selected = [target_names[0], target_names[1]]
+
+# Create EasySurrogate campaign
+campaign = es.Campaign()
+
+# Load HDF5 data frame
+data_file_name = f"gem04_f{ft}.hdf5"
+data_frame = campaign.load_hdf5_data(file_path='../../../'+data_file_name)
+# TODO: get rid of hardcoding relative path
+
+# Supervised training data set
+features = [data_frame[k] for k in features_names_selected if k in data_frame]
+target = np.concatenate([data_frame[k] for k in target_name_selected if k in data_frame], axis=1)
+
+# Create an ANN surrogate
+surrogate = es.methods.ANN_Surrogate(
+    #backend=str(inputs['backend']),
+    n_in=len(features_names_selected),
+    n_out=len(target_name_selected),
+    )
+
+#TODO mind if need to restore default value for a parameter
+surrogate.train(
+    features, 
+    target,  
+    n_iter=int(inputs['n_iter']),
+    test_frac=float(inputs['testset_fraction']),
+    n_layers=int(inputs['n_layers']),
+    n_neurons=int(inputs['n_neurons']),
+    batch_size=int(inputs['batch_size']),
+    activation=str(inputs['activation']),
+    )
+
+surrogate.neural_net.print_network_info()
+
+campaign.add_app(name='ann_campaign', surrogate=surrogate)
+campaign.save_state(file_path='model.pickle')
+
+# Performing surrogate analysis, here: measuring perfromance on testing/validation data
+feat_train, targ_train, feat_test, targ_test = campaign.surrogate.feat_eng.\
+    get_training_data(features, target, index=campaign.surrogate.feat_eng.train_indices)
+
+analysis = es.analysis.ANN_analysis(ann_surrogate=campaign.surrogate)
+
+err_train, err_test = analysis.get_errors(
+    feat_test,
+    targ_test,
+    #feat_train,
+    #targ_train,
+    #flag_plot=True,
+    )
+print(f"err_test = {err_test}") ###DEBUG
+
+#err_test_tot = [float(np.abs(err).mean()) for err in err_test]
+#loss_list = [1. - r2 for r2 in r2_test]
+#loss = sum(loss_list) / len(loss_list)
+
+loss = err_test
+
+# Writing an output
+output = {'loss': loss}
+with open('output.json', 'w') as of:
+    json_string = json.dumps(output)
+    of.write(json_string)
+
+print(f"Outputs: {output}")
+
+print(f"> Exiting the training script")
---
+++ tests/hpo/train_ann.py
@@ -0,0 +1,205 @@
+"""
+Run a EasyVVUQ with a surrogate trainging as an application.
+Varying parameters are the Hyperparameters of the surrogate models.
+"""
+
+import os
+import sys
+import pickle
+import time
+
+import easyvvuq as uq
+from easyvvuq.actions import QCGPJPool
+from easyvvuq.actions.execute_qcgpj import EasyVVUQParallelTemplate
+
+from qcg.pilotjob.executor_api.qcgpj_executor import QCGPJExecutor
+
+from pprint import pprint
+from itertools import product
+import csv
+import numpy as np
+import math as m
+
+import easysurrogate as es
+
+#TODO: ADD A RANDOM SEED
+rs = 42
+np.random.seed(rs)
+
+# Choice of flux tube
+ft = sys.argv[1]
+
+print(f"> Starting a hyperparameter optimisation for flux tube {ft}, random_seed={rs}")
+
+#TODO write down test fraction expliceitly - now it is 0.2 by default
+
+# List all possible hyperparamters of a surrogate of this type, together with their types and default values
+params = {
+    #"length_scale": {"type": "string", "min": 1e-12, "max": 1e+12, "default": "1.0"},
+    #"noize": {"type": "string", "min": 1e-16, "max": 1e+4, "default": "1e-3"}, 
+    #"bias": {"type": "string", "min": -1e+6, "max": 1e+6, "default": "0.0"},
+    #"backend" : {"type": "string", "default": "local"},
+    "testset_fraction": {"type": "string", "min": 0.0, "max": 1.0, "default": "0.2"},
+    "n_iter" : {"type": "string", "min": 1, "default": "10"},
+    "n_layers" : {"type": "string", "min": 1, "default": "2"},
+    "n_neurons" : {"type": "string", "min": 1, "default": "16"},
+    "batch_size" : {"type": "string", "min": 1, "default": "32"},
+    "activation" : {"type": "string", "default": "relu"},
+}
+# looks like EasyVVUQ checks for a need in a default value after sampler is initialized 
+
+# TODO should be read from CSV
+# TODO force CSVSampler to interpret entries with correct type (int-s as int-s!)
+
+# For Grid Search: form carthesian product of variables
+
+vary = {} #TODO maybe: use vary to create CSV for non-categorical hyperparameters
+
+# Define values for each parameter, create its cartesian grid, save as csv
+param_search_vals = {
+    #"length_scale": [0.5, 1.0, 2.0], # not used
+    #"noize": [1e-4, 1e-2, 1e-1], # not used
+    #"bias": [0., 1.0], # not used
+    #"backend" : ['local'] # ['local', 'scikit-learn'], # not used
+    "n_iter" : [1000, 2500, 5000, 10000, 20000, 30000],
+    "n_layers" : [2, 3, 4, 5],
+    "n_neurons" : [8, 16, 32, 64, 128],
+    "batch_size" : [8, 16, 32, 64, 128],
+    "activation" : ['relu', 'tanh', 'sigmoid'],
+}
+
+csv_header = [k for k in param_search_vals.keys()]
+csv_vals = [x for x in product(*[v for (k,v) in param_search_vals.items()])]
+
+def clean_grid_by_rules(header, vals, def_vals):
+
+    # Construct list of dictionaries of with keys taken from header and values taken from rows of vals
+    data = [{header[i]:vals[j][i] for i in range(len(header))} for j in range(len(vals))]
+
+    vals_new = []
+
+    for d in data:
+        pass
+        vals_new.append([x for k,x in d.items()])
+
+    print('> Using {0} different parameter values combinations instead of full {1}'.format(len(vals_new), len(vals)))
+
+    return vals_new
+
+csv_defaults = {k:v['default'] for k,v in params.items()}
+#csv_vals_new = clean_grid_by_rules(csv_header, csv_vals, csv_defaults)
+
+with open('hp_values_ann_loc_1.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(csv_header)
+    writer.writerows(csv_vals)
+
+#with open('hp_values_ann_loc_short_1.csv', 'w') as f:
+#    writer = csv.writer(f)
+#    writer.writerow(csv_header)
+#    writer.writerows(csv_vals_new)
+ 
+# If run on HPC, should be called from the scheduler like SLURM
+# for which an environmental variable HPC_EXECUTION should be specified
+HPC_EXECUTION = os.environ['HPC_EXECUTION']
+
+campaign_name = f"hpo_easysurrogate_ann_f{ft}_{time.strftime('%Y%m%d_%H%M%S', time.localtime())}_"
+work_dir = ''
+#TODO specify flexible paths
+
+campaign = uq.Campaign(name=campaign_name, work_dir=work_dir)
+
+# Optimising hyperparamters for GEM data
+# Here listing different test cases for different combinations of varied parameters
+
+# A file specifying a table of hyperparameter values (or their ranges/distributions) to pass for a number of ML models
+# Mind: delimeter is ',' w/o spaces
+
+param_file = 'hp_values_ann_loc_1.csv'
+
+# Encoder should take a value from the sampler and pass it to EasySurrogate es.methos.*_Surrogate().train(...) as kwargs
+encoder = uq.encoders.GenericEncoder(
+    template_fname='hpo_ann.template',
+    delimiter='$',
+    target_filename='input.json'
+)
+
+# Decoder should take a training, or better validation, loss values from an ML model for given hyperparameter value
+qoi = ['loss']
+decoder = uq.decoders.JSONDecoder(
+    target_filename='output.json',
+    output_columns=qoi,
+)
+
+# Execute should train a model in EasySurrogate: get the data, initalise object, call .train() and calculate the training/validation error
+execute_train = uq.actions.ExecuteLocal(
+    f"python3 ../../../single_model_train_ann.py input.json {ft} > train.log"
+)
+# TODO get rid of hard-coding relative paths
+
+actions = uq.actions.Actions(
+    uq.actions.CreateRunDirectory('/runs', flatten=True),
+    uq.actions.Encode(encoder),
+    execute_train,
+    uq.actions.Decode(decoder),
+)
+
+campaign.add_app(
+    name=campaign_name,
+    params=params, # TODO read from CSV
+    actions=actions,
+)
+
+# Sampler should read hyperparameter-value dictionary from a CSV file
+sampler = uq.sampling.CSVSampler(filename=param_file) 
+# TODO: sampler has to read numbers as integers
+campaign.set_sampler(sampler)
+
+# Execute: train a number of ML models
+print(f"> Starting to train the models")
+start_time = time.time()
+
+if HPC_EXECUTION:
+
+    with QCGPJPool(
+            #qcgpj_executor=QCGPJExecutor(),
+            template=EasyVVUQParallelTemplate(),
+            template_params={
+                'numCores':1           
+            }
+        ) as qcgpj:
+        
+        print(f">> Executing on a HPC machine")
+        campaign.execute(pool=qcgpj).collate()
+else:
+
+    campaign.execute().collate()
+
+train_time=time.time() - start_time
+print(f"> Finished training the models, time={train_time} s")
+
+# Collate the results of all training runs
+collation_results = campaign.get_collation_result()
+pprint(collation_results)
+
+# TODO Next: analysis, create a separate class to choose the best ML model
+analysis = uq.analysis.BasicStats(qoi_cols=qoi)
+campaign.apply_analysis(analysis)
+results = campaign.get_last_analysis()
+
+res_file = os.path.join(work_dir, f"hpo_res_{ft}.pickle")
+with open(res_file, "bw") as rf:
+    pickle.dump(results, rf)
+
+#print(results)
+
+analysis.analyse(collation_results)
+analysis.analyse(results)
+
+minrowidx = collation_results['loss'].idxmin()
+print(f"Best model (for ft#{ft}) so far: {collation_results.iloc[minrowidx,:]}")
+
+#TODO: get folder name of the current campaign and copy the right model file out
+
+#loss = results.describe('loss')
+#print(loss)
