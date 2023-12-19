247d1bd7b608614d7f39ef73428ccafbd3d4dfbb
Yehor Yudin <yehor.yudin@ipp.mpg.de>
2023-12-19 18:20:36 +0100

    Fusion Turbulence: new workflow shell scripts

---
+++ tests/hpo/copy_hpo_runs.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Copy results of HPO (the best model) into a new subdirectory
+
+MODELDESCRIPTOR=gem0_es_ann_
+
+LASTDATE=20231218
+
+# define destination folder
+#NEWFOLDER=20231130_models
+#NEWFOLDER=20231215_models
+NEWFOLDER=${LASTDATE}_models #ANN run on GEM0 data on same grid
+
+mkdir ${NEWFOLDER}
+
+# define source folders
+#FOLDERPREFIX=hpo_easysurrogate_f
+FOLDERPREFIX=hpo_easysurrogate_ann_f
+
+# Next could be read from 'ls -d ${FOLDERPREFIX}[0-9] some date...'
+#FOLDERSUFFIX='_20231130_14'
+FOLDERSUFFIX='_2023121' #ANN run on GEM0 data on same grid
+
+
+#FOLDERS=('2559_wmbdq3l9' '2932__e9q67cj' '3307_dca3cv55' '3636_hmrr9kv5' '4010_7nov_o0r' '4343_3piogul4' '4716_6m7eajmq' '5051__ngeq4gb')
+#FOLDERS=('4_115046_faoiidv8' '4_120715__o62xru9' '3_204322_f2mqqw4t' '4_123209_2hlb8yxh' '4_124834_po82zy4m' '4_130458_914zk24y' '4_132121_b1vm8cxy' '4_133747_gdihum57')
+FOLDERS=('8_171046_ooygaf0e' '8_174205_bucn60_s' '8_181329_yjqxti7u' '8_184451_3n2p1x0p' '8_191608_s6v_qq7g' '8_194728_yy8yeex_' '8_201855_0alnuw9m' '9_152226_hksn9jae') #ANN run on GEM0 data on same grid
+
+
+# copy the best model from each folder
+# Next could be parsed from a submission log file
+#RUNS=('160' '163' '151' '151' '151' '151' '163' '151')
+#RUNS=('1794' '1797' '1794' '1797' '1797' '1794' '1797' '1797')
+RUNS=('2792' '2396' '2392' '2796' '2796' '2392' '2392' '2792') #ANN run on GEM0 data on same grid
+
+
+for i in ${!FOLDERS[@]}; do 
+  cp ${FOLDERPREFIX}${i}${FOLDERSUFFIX}${FOLDERS[$i]}/runs/run_${RUNS[$i]}/model.pickle ${NEWFOLDER}/${MODELDESCRIPTOR}${i}.pickle
+done
+
+
+# perform new analysis on the best models to generate scans 
+TARGETFOLDER=../../../MFW/uq/basicda
+cd ../gem_gp
+
+for((i=0;i<8;i++)); do 
+  cp ../hpo/${NEWFOLDER}/${MODELDESCRIPTOR}${i}.pickle ./gem0_es_ann_${i}_${LASTDATE}.pickle
+  python test_model_ann.py ${i} ${LASTDATE}
+  cp scan_${i}.csv ${TARGETFOLDER}/scan_gem0surr_${LASTDATE}_ft${i}.csv
+done
+
---
+++ tests/hpo/run_cobra_hpo_ann.sh
@@ -0,0 +1,67 @@
+#!/bin/bash -l
+
+## job name
+#SBATCH --job-name=HPO_ANN_
+
+## stdout and stderr files
+#SBATCH --output=hpo-ann-out.%j
+#SBATCH --error=hpo-ann-err.%j
+
+## wall time in format (HOURS):MINUTES:SECONDS
+#SBATCH --time=4:30:00
+
+## number of nodes and tasks per node
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+
+#SBATCH --partition=medium
+###SBATCH --qos=
+
+## grant
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yyudin@ipp.mpg.de
+
+######################################
+# Loading modules
+module load anaconda/3/2021.11 
+
+# Python set-up
+source activate $HOME/conda-envs/python3114
+
+export SYS=COBRA
+export SCRATCH=$SCRATCH
+
+# For QCG-PilotJob usage
+export ENCODER_MODULES
+export EASYPJ_CONFIG=conf.sh
+
+export HPC_EXECUTION=1
+
+echo -e '> In this run: use ExecuteLocal only + QCGPJ pool + '$SLURM_NNODES' nodes /n'
+
+####################################
+pip install latexplotlib
+
+# Update the package
+cd ../..
+pip install .
+cd tests/hpo
+
+# Define the flux tube number
+FT_LEN=8
+
+
+#for((i=0;i<${FT_LEN};i++)); do
+for((i=7;i<${FT_LEN};i++)); do
+
+    DATAFILE=gem05_f${i}.hdf5
+
+    # Run the training code
+    echo '> Running the training code for flux tube #'${i}' and datafile '${DATAFILE}
+
+    python3 train_ann.py ${i} >> hpo-ann-log.${SLURM_JOBID}
+
+done;
+
+echo '...'
+echo "> Finished a SLURM job for HPO!"
